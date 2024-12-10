0fa4aba3eed4c15ef2b5bb01eaae74a1a9a2d662
erlingrj <erling.jellum@gmail.com>
2024-12-09 22:32:35 -0800

    Remove old test script

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-set -e
-
-for FILE in src/*.lf; do
-  if [ -f $FILE ]; then
-    cmake --fresh -Bbuild -DLF_APP=$(basename "$FILE" .lf) .
-    cmake --build build --parallel 12
-  fi
-done
